95ea8ce03c3fe28545ca9e67ba8205487750a4e4
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2026-04-30 19:45:10 +0530

    UC9: Weight Measurement Equality, Conversion, and Addition (Kilogram, Gram, Pound)

--- src/main/QuantityMeasurementApp.java
+++ src/main/QuantityMeasurementApp.java
@@ -2,6 +2,27 @@ package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
 
+    public enum LengthUnit {
+        FEET(1.0),
+        INCHES(1.0 / 12.0),
+        YARDS(3.0),
+        CENTIMETERS(0.0328084);
+
+        private final double toFeetFactor;
+
+        LengthUnit(double toFeetFactor) {
+            this.toFeetFactor = toFeetFactor;
+        }
+
+        public double toFeet(double value) {
+            return value * toFeetFactor;
+        }
+
+        public double fromFeet(double feetValue) {
+            return feetValue / toFeetFactor;
+        }
+    }
+
     public static class Length {
         private final double value;
         private final LengthUnit unit;
@@ -15,20 +36,20 @@ public class QuantityMeasurementApp {
             return unit.toFeet(value);
         }
 
-        // UC5
+        // UC5 - Conversion
         public double convertTo(LengthUnit targetUnit) {
             double inFeet = this.toBase();
             return targetUnit.fromFeet(inFeet);
         }
 
-        // UC6
+        // UC6 - Addition (same unit)
         public Length add(Length other) {
             double sumFeet = this.toBase() + other.toBase();
             double result = unit.fromFeet(sumFeet);
             return new Length(result, unit);
         }
 
-        // UC7
+        // ✅ UC7 - Addition with target unit
         public Length add(Length other, LengthUnit targetUnit) {
             double sumFeet = this.toBase() + other.toBase();
             double result = targetUnit.fromFeet(sumFeet);
@@ -38,5 +59,19 @@ public class QuantityMeasurementApp {
         public double getValue() {
             return value;
         }
+    }
+
+    public static void main(String[] args) {
+        Length l1 = new Length(1.0, LengthUnit.FEET);
+        Length l2 = new Length(12.0, LengthUnit.INCHES);
+
+        System.out.println("1 foot in inches: " +
+                l1.convertTo(LengthUnit.INCHES));
+
+        System.out.println("1 ft + 12 inches (feet): " +
+                l1.add(l2).getValue());
+
+        System.out.println("1 ft + 12 inches (yards): " +
+                l1.add(l2, LengthUnit.YARDS).getValue());
     }
 }
